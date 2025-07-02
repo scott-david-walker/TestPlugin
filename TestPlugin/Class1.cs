c1318242bfb059a045894e1b0bbf3587de6310c9
Scott Walker <scott.david.walker@hotmail.com>
2025-07-02 10:39:05 +0200

    init

---
+++ TestPlugin/Class1.cs
@@ -0,0 +1,44 @@
+﻿using KlaarDesk.Plugins;
+using Newtonsoft.Json;
+
+namespace TestPlugin;
+
+public class FakePlugin : IKlaarDeskChannelPlugin
+{
+    public string Name { get; } = "Fake Slack Plugin";
+    private PluginChannelContext _context;
+
+    public class TESTSTS
+    {
+        public string Name { get; set; } = "Test";
+    }
+    public void Initialise(PluginChannelContext context)
+    {
+        _context = context;
+    }
+
+    public Dictionary<string, string> ConfigurationFields()
+    {
+        return new();
+    }
+
+    public bool ValidateConfiguration()
+    {
+        return true;
+    }
+
+    public void TicketUpdated()
+    {
+        var x = new TESTSTS();
+        Console.WriteLine("Serialising object to JSON:");
+        var p = JsonConvert.SerializeObject(x);
+        Console.WriteLine(p);
+        File.WriteAllText("./test.json", p);
+        var result = _context.TicketRepository.AddReply();
+        Console.WriteLine(result);
+    }
+
+    public void LookForTicketsAndReplies()
+    {
+    }
+}
